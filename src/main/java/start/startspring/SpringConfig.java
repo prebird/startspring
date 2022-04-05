7623d22a4e3b7efbf8a15b60ae88c6d0ce694d2b
yonggyujeong <E19hiNk2@>
2022-04-05 23:55:06 +0900

    jdbctemplete

--- src/main/java/start/startspring/SpringConfig.java
+++ src/main/java/start/startspring/SpringConfig.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import start.startspring.repository.JdbcMemberRepository;
+import start.startspring.repository.JdbcTempleteMemberRepository;
 import start.startspring.repository.MemberRepository;
+import start.startspring.repository.MemoryMemberRepository;
 import start.startspring.service.MemberService;
 
 import javax.sql.DataSource;
@@ -13,7 +15,7 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
@@ -26,8 +28,9 @@ public class SpringConfig {
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        // return new MemoryMemberRepository();
+    public MemberRepository memberRepository(){ // 어떤 리포지토리를 사용할지 빈에 직접 등록
+        //return new JdbcTempleteMemberRepository(dataSource);
+        //return new MemoryMemberRepository();
         return new JdbcMemberRepository(dataSource);
 
     }
